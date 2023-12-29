c56099e65b6eea5822e60597bc3ffdb03d68c4c4
Rushikesh Barkade <143509436+rushikeshbarkadecode@users.noreply.github.com>
2023-12-29 09:52:38 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import ProductCard from './components/ProductCard'
+import HeroSection from './components/HeroSection'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <HeroSection></HeroSection>
+      <ProductCard></ProductCard>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/HeroSection.jsx
@@ -0,0 +1,8 @@
+export default function HeroSection(){
+    return(
+        <div className="hero-section">
+            <h2>Elevate your soul with handmade watches</h2>
+            <button>See latest watches</button>
+        </div>
+    )
+}
