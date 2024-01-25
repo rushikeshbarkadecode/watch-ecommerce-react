1160a615e7bc6418510966c1a8de23f8a0e25b14
Rushikesh Barkade <143509436+rushikeshbarkadecode@users.noreply.github.com>
2024-01-25 08:26:33 +0530

    improved product page

--- src/pages/product.jsx
+++ src/pages/product.jsx
@@ -4,7 +4,16 @@ export default function Product(){
     const {productName} = useParams()
     return(
         <div className='page'>
-            <h2>Product Name: {productName}</h2>
+            <div className="product-container">
+                <div className="img-section">Img</div>
+                <div className="product-info">
+                    <h1>{productName}</h1>
+                    <h4>Price: $200</h4>
+                    <p>This is product description section.</p>
+                    <button className="add-to-cart">Add to cart</button>
+                    <button className="buy-now">Buy now</button>
+                    </div>
+            </div>
         </div>
     )
 }
