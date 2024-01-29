418e6d7e996a3c6a1c98c70bbe8c43df9a25972f
Rushikesh Barkade <143509436+rushikeshbarkadecode@users.noreply.github.com>
2024-01-29 10:15:20 +0530

    updated limited offer card

--- src/components/LimitedOffer.jsx
+++ src/components/LimitedOffer.jsx
@@ -4,7 +4,7 @@ export default function LimitedOffer(){
     return(
         <div className="limited-offer">
         <div className="col1">
-            <ProductCard title="Rolex Tenet" price="599"></ProductCard>
+            <ProductCard title="Rolex Submariner" price="25000" img='/static/images/rolex-submariner.webp'></ProductCard>
         </div>
         <div className="col2">
             <h3>Limited Period Offer</h3>
