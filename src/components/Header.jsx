aa5dacf8520218497405174fc121d1c0d2f3de67
Rushikesh Barkade <143509436+rushikeshbarkadecode@users.noreply.github.com>
2024-05-22 08:55:41 +0530

    Updated header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,6 @@
 import { useRef, useState } from "react"
 import { Link } from "react-router-dom"
+import HeaderSearch from "./HeaderSearch"
 
 export default function Header(){
     let menuVisibility = false
@@ -17,29 +18,28 @@ export default function Header(){
     return(
         <header className="header">
             <nav>
-            <button onClick={toggleMenu} className="menu-icon">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" className="bi bi-list" viewBox="0 0 16 16">
-                        <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5"/>
-                    </svg>    
-                </button>
-                <div className="logo-container">
-                    <Link to='/'><img src="/static/images/logo.png" alt="PotentWatch" /></Link>
-                </div>
-                <div ref={ref} id="menu">
-                    <div>
-                        <ul>
-                            <li><Link to='/account'>Account</Link></li>
-                            <li><Link to='/cart'>Cart</Link></li>
-                            <li><Link to='/orders'>Orders</Link></li>
-                            <li><Link to='/login'>Login</Link></li>
-                            <li><Link to='/sign-up'>Sign Up</Link></li>
-                        </ul>
+                <div className="upper-header">
+                    <button onClick={toggleMenu} className="menu-icon">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" className="bi bi-list" viewBox="0 0 16 16">
+                            <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5"/>
+                        </svg>
+                    </button>
+                    <div className="logo-container">
+                        <Link to='/'><img src="/static/images/logo.png" alt="PotentWatch" /></Link>
+                    </div>
+                    <div ref={ref} id="menu">
+                        <div>
+                            <ul>
+                                <li><Link to='/account'>Account</Link></li>
+                                <li><Link to='/cart'>Cart</Link></li>
+                                <li><Link to='/orders'>Orders</Link></li>
+                                <li><Link to='/login'>Login</Link></li>
+                                <li><Link to='/sign-up'>Sign Up</Link></li>
+                            </ul>
+                        </div>
                     </div>
                 </div>
-                <div className="search-container">
-                    <input type="text" placeholder="Search for any watch"/>
-                    <button>Search</button>
-                </div>
+                <HeaderSearch></HeaderSearch>
             </nav>
         </header>
     )
---
+++ src/components/HeaderSearch.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function HeaderSearch() {
+  return (
+    <div className="header-search">
+        <input type="text" placeholder="Search for any watch"/>
+        <button>Search</button>
+    </div>
+  )
+}
